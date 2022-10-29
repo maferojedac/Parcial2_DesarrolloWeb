becb22fe4de540e81dc5c1f715c82275bebdd5a4
Maria Fernanda Ojeda Carrillo <0228660@up.edu.mx>
2022-10-29 13:57:19 -0500

    perdon, no me salio, pero lo intente :((((

--- controller.js
+++ controller.js
@@ -7,7 +7,7 @@ const getUserData = async (req, res) => {
         const {params : {id}} = req;
 
         //x-lambda funcion, se ejecuta n numero de veces
-       const currentUser = userData.find(x => x.id === id)
+       const currentUser = userData.find(x => x.id == id)
         
         const {
             firstName,
@@ -16,6 +16,7 @@ const getUserData = async (req, res) => {
             age, 
             email, 
             address, 
+            company
         } = currentUser;
 
 //teniamos que ver los datos de bd y ver los datos que necesitamos
@@ -32,7 +33,9 @@ const getUserData = async (req, res) => {
             }
         });
     } catch (error) {
+        console.log(error);
         res.send(error);
+        
     }
 }
 
@@ -40,7 +43,7 @@ const getUserData = async (req, res) => {
 const updateUserAddress = async(req,res) => {
     try {
         const {params : {id}, body: newAddress} = req;
-       const currentUser = userData.find(x => x.id === id)
+       const currentUser = userData.find(x => x.id == id)
        const user = {...currentUser, address: newAddress} //sobrescribe
        
        res.send({
@@ -48,13 +51,59 @@ const updateUserAddress = async(req,res) => {
         user
        });
     } catch (error) {
+        console.log(user)
+        console.log(error)
+        res.send(error)
+    }
+}
+
+const createUser = async (req, res) => {
+    try {
+        const {body: userData} = req;
+        //se que esto ni va a jalar, pero lo que queria era que buscara el ultimo id y que a ese le sumara 1 para generar un nuevo id
+        const newID = userData.find(x => x.id == id) + 1
+        const newUser = await userData.push({
+            newID,
+            email: "newEmail@something.com"
+        }) 
+
+        res.send({
+            status: 200,
+            users: [
+                userData,
+                newUser
+            ]
+        })
+    }
+
+    catch (error) {
+        console.log(error)
+        res.send(error)
+    }
+}
+
+
+const deleteUser = async (req, res) => {
+    try {
+        const {params: {id}}= req;
+        const deletedUser = userData.findIndex(x => x.id == id)
+        //const deletions = await userData.delete(deletedUser)
+        userData.splice(deletedUser,1) //borramos solo 1 elemento
+
+        res.send({
+            status: 200,
+            users
+        })
+
+    } catch (error) {
+        console.log(error)
         res.send(error)
     }
 }
 
 
 module.exports = {
-    getUserData, updateUserAddress
+    getUserData, updateUserAddress, createUser, deleteUser
 }
 
 //crear una nueva rama "perdon"
